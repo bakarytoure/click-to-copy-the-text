15cde62a4d35ced84e52bf5f16ebf22d9a24bfae
bakarytoure <bakarytoure57@gmail.com>
2020-07-10 16:12:39 +0000

    Updates

---
+++ precache-manifest.70f6e51d6d937fb75696e7612bd4e9a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41f86d4d7e1249c765d26f4c31f7bc4",
+    "url": "/click-to-copy-the-text/index.html"
+  },
+  {
+    "revision": "ca7dbf26d9c4fecfa7e4",
+    "url": "/click-to-copy-the-text/static/css/main.2a9d6512.chunk.css"
+  },
+  {
+    "revision": "09180fa2a4b91fec2ec6",
+    "url": "/click-to-copy-the-text/static/js/2.aa748b6e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/click-to-copy-the-text/static/js/2.aa748b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7dbf26d9c4fecfa7e4",
+    "url": "/click-to-copy-the-text/static/js/main.61e483fc.chunk.js"
+  },
+  {
+    "revision": "18d1537180ceecd51622",
+    "url": "/click-to-copy-the-text/static/js/runtime-main.7a1eed65.js"
+  }
+]);
